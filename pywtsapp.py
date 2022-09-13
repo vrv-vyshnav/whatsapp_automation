510d02038927b5bfc390b1e5cf430365cc0d31f5
Vaishnav <vyshnavvishnu707@gmail.com>
2022-09-13 03:12:30 -0400

    status addeed

--- pywtsapp.py
+++ pywtsapp.py
@@ -17,7 +17,8 @@ class pywhtsapp:
         options.add_experimental_option(
             "excludeSwitches", ["enable-automation"])
         options.add_experimental_option("useAutomationExtension", False)
-        # path = '/home/vaishnav/chromedriver'
+
+        # options.add_argument("--headless") # To run in background
 
         service = ChromeService(executable_path=path)
         driver = webdriver.Chrome(service=service, options=options)
@@ -32,21 +33,30 @@ class pywhtsapp:
                 EC.presence_of_element_located((loc))
             )
             return True
+        except KeyboardInterrupt:
+            print("[ "+'\033[91m' + "FAILED" +
+                  "\033[0m" + " ] Keyboard Interrupt ")
         except:
             return (self.pageLoadCheck(loc, 2.5))
 
     def usercheck(self, username):
-        # selecting the username
-        username_box = By.XPATH, "//div[@title='Search input textbox']"
-        chkLoaded = self.pageLoadCheck(loc=username_box, _time=15)
-        username_box = str(username_box)
-        if chkLoaded:
-            search_box = driver.find_elements(
-                By.XPATH, "//div[@title='Search input textbox']")
-            search_box[0].send_keys(username)
-            search_box[0].send_keys(Keys.ENTER)  # press ENTER
+        try:
+            # selecting the username
+            username_box = By.XPATH, "//div[@title='Search input textbox']"
+            chkLoaded = self.pageLoadCheck(loc=username_box, _time=15)
+
+            username_box = str(username_box)
+            if chkLoaded:
+                search_box = driver.find_elements(
+                    By.XPATH, "//div[@title='Search input textbox']")
+                search_box[0].send_keys(username)
+                search_box[0].send_keys(Keys.ENTER)  # press ENTER
+                return "[ "+'\033[92m' + "Ok" + "\033[0m" + " ] User Found"
+        except:
+            print("[ "+'\033[91m' + "FAILED" + "\033[0m" + " ] User Not Found")
 
     def send_message(self, text, user):
+        # Sending Message
         try:
             msg_box = By.CLASS_NAME, "p3_M1"
             self.usercheck(username=user)
@@ -57,15 +67,17 @@ class pywhtsapp:
                 textbox[0].send_keys(Keys.ENTER)  # press ENTER
                 return "[ "+'\033[92m' + "OK" + "\033[0m" + " ] Message Send Successfully"
         except:
-            return "[ "+'\033[91m' + "FAILED" + "\033[0m" + " ] Messge send Failed"
+            print("[ "+'\033[91m' + "FAILED" +
+                  "\033[0m" + " ] Messge send Failed")
 
-    def send_attachment(self, file_path, username):
+    def send_attachment(self, file_path, user):
+        # Sending attachements video / audio / pdf
         try:
             Attach_button = By.XPATH, "//div[@title='Attach']"
             video_audio_img = By.XPATH, "//input[@accept='image/*,video/mp4,video/3gpp,video/quicktime']"
             file_send_button = By.XPATH, "//span[@data-icon='send']"
 
-            self.usercheck(username)
+            self.usercheck(user)
             self.pageLoadCheck(Attach_button, 1)
             attach = driver.find_elements(By.XPATH, "//div[@title='Attach']")
             attach[0].click()
@@ -79,23 +91,29 @@ class pywhtsapp:
             submit_button[0].click()
             return "[ "+'\033[92m' + "OK" + "\033[0m" + " ] File Send Successfully"
         except:
-            return "[ "+'\033[91m' + "FAILED" + "\033[0m" + " ] File send Failed"
+            print("[ "+'\033[91m' + "FAILED" +
+                  "\033[0m" + " ] File Send Failed")
 
-    def text_to_audio(self, text, username):
-        self.text = text
-        self.username = username
-        time_class = datetime.now()
-        date_time = time_class.strftime("%d-%m-%Y,%H:%M:%S")
-        parent_directory = os.getcwd()
-        path = os.path.join(parent_directory, "Sounds/")
-        if not os.path.exists("Sounds"):
-            os.mkdir(path)
-        language = 'en'
-        myobj = gTTS(text=text, lang=language, slow=False)
-        myobj.save("%s.mp3" % os.path.join(path, str(date_time)))
-        path_of_sound = path + date_time + ".mp3"
-        self.send_attachment(path_of_sound, username)
+    def text_to_audio(self, text, user):
+        try:
+            # Convert the text to audio and send to whatsapp
+            time_class = datetime.now()
+            date_time = time_class.strftime("%d-%m-%Y,%H:%M:%S")
+            parent_directory = os.getcwd()
+            path = os.path.join(parent_directory, "Sounds/")
+            if not os.path.exists("Sounds"):
+                os.mkdir(path)
+            language = 'en'
+            myobj = gTTS(text=text, lang=language, slow=False)
+            myobj.save("%s.mp3" % os.path.join(path, str(date_time)))
+            path_of_sound = path + date_time + ".mp3"
+            self.send_attachment(path_of_sound, user)
+        except:
+            print("[ "+'\033[91m' + "FAILED" +
+                  "\033[0m" + " ] Error converting to Audio ")
 
     def close(self, _time=10):
+        print("[ "+'\033[92m' + "OK" + "\033[0m" +
+              " ] The Browser will close in ", _time, " sec")
         time.sleep(_time)
         driver.close()
